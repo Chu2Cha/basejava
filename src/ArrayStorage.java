84fe51aadd62710aeed43578b0b89ae3bf356820
Chu2Cha <103102413+Chu2Cha@users.noreply.github.com>
2022-11-08 14:44:19 +0300

    Update ArrayStorage.java
    1. get -kludge (noSearch = null). Как вернуть исключение вместо объекта? Создавать объект Resume с error-текстом как-то глупо.
    2. Сомнительное решение с boolean проверкой поиска. Ради того, чтобы 2 раза не писать сравнение в get и delete, появляется конструкция типа if (checkSearchUuid(searchUuid(uuid))). Это точно "чистый код"?
    3. Версия залита  вручную, не всегда есть возможность пользоваться git,

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -3,65 +3,71 @@
  */
 public class ArrayStorage {
     Resume[] storage = new Resume[10000];
-    int storageSize = 0;
+    int returnArrayLength = 0;
 
     void clear() {
         for (int i = 0; storage[i] != null; i++) {
             storage[i] = null;
-            storageSize--;
+            returnArrayLength--;
         }
-    }
-
-    Resume[] createReturnArray() {
-        Resume[] returnArray = new Resume[storageSize];
-        for (int i = 0; i < storageSize; i++) {
-            returnArray[i] = storage[i];
-        }
-        return returnArray;
     }
 
     void save(Resume r) {
-
         for (int i = 0; i < storage.length; i++) {
             if (storage[i] == null) {
                 storage[i] = r;
-                storageSize++;
+                returnArrayLength++;
                 break;
             }
         }
     }
 
-    Resume get(String uuid) {
-        return createReturnArray()[findUuid(uuid)];
+    int searchUuid(String uuid) {
+        int searchResult = -1;
+        for (int i = 0; i < returnArrayLength; i++) {
+            if (createReturnArray()[i].toString().equals(uuid)) {
+                searchResult = i;
+            }
+        }
+        return searchResult;
     }
 
-    void delete(String uuid) {
-        for (int i = findUuid(uuid); storage[i] != null; i++) {
-            storage[i] = storage[i + 1];
+    boolean checkSearchUuid(int searchResult) { 
+        return searchResult >= 0;
+    }
+
+    Resume get(String uuid) {
+        if (checkSearchUuid(searchUuid(uuid))) 
+            return createReturnArray()[searchUuid(uuid)];
+        else
+            return null; //Как 
+    }
+
+    void delete(String uuid) {  
+        if (checkSearchUuid(searchUuid(uuid))) {
+            for (int i = searchUuid(uuid); storage[i] != null; i++) {
+                storage[i] = storage[i + 1];
+            }
+            returnArrayLength--;
         }
-        storageSize--;
     }
 
     /**
      * @return array, contains only Resumes in storage (without null)
      */
+    Resume[] createReturnArray() {
+        Resume[] returnArray = new Resume[returnArrayLength];
+        for (int i = 0; i < returnArrayLength; i++) {  //IDEA предлагает System.arraycopy, я такое еще не умею
+            returnArray[i] = storage[i];
+        }
+        return returnArray;
+    }
 
     Resume[] getAll() {
-        createReturnArray();
         return createReturnArray();
     }
 
     int size() {
-        return storageSize;
-    }
-
-    int findUuid(String uuid) {
-        int result = 0;
-        for (int i = 0; i < storageSize; i++) {
-            if (createReturnArray()[i].toString().equals(uuid)) {
-                result = i;   //TODO exception
-            }
-        }
-        return result;
+        return returnArrayLength;
     }
 }
